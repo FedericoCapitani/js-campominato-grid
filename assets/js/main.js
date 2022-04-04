534c860852e20865a17f803fb3470fe6dbdee309
FedericoCapitani <federico.capitani@outlook.it>
2022-04-04 15:34:05 +0200

    creazione griglie 81 e 49

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,48 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
 let gioca = document.getElementById('gioca');
 gioca.addEventListener('click', function(){
 
+    let difficolta = document.getElementById('difficolta').value
+    console.log(difficolta);
+    if(difficolta == 1){
     // celle difficoltà 1
     function generateGrid(selector, tag_name, class_name){
         const cellsElement = document.querySelector(selector)
     
-        for (let i = 1; i < 100; i++){
+        for (let i = 1; i <= 100; i++){
             const cellItem = document.createElement(tag_name);
             cellItem.classList.add(class_name);
             cellsElement.append(cellItem);
         }
     }
     
-    generateGrid('.cells', 'div', 'cell');
+    generateGrid('.cells', 'div', 'cell_100');
+    }else if (difficolta == 2){
+    // celle difficoltà 2
+    function generateGrid(selector, tag_name, class_name){
+        const cellsElement = document.querySelector(selector)
+    
+        for (let i = 1; i <= 81; i++){
+            const cellItem = document.createElement(tag_name);
+            cellItem.classList.add(class_name);
+            cellsElement.append(cellItem);
+        }
+    }
+    
+    generateGrid('.cells', 'div', 'cell_81');
 
-})
+    }else if (difficolta == 3){
+    // celle difficoltà 3
+    function generateGrid(selector, tag_name, class_name){
+        const cellsElement = document.querySelector(selector)
+    
+        for (let i = 1; i <= 49; i++){
+            const cellItem = document.createElement(tag_name);
+            cellItem.classList.add(class_name);
+            cellsElement.append(cellItem);
+        }
+    }
+    
+    generateGrid('.cells', 'div', 'cell_49');
+    }
+});
 
